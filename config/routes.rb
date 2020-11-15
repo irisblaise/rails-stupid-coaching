7c76e8dc31edd771192c75d3e8d1a574c317d99d
Iris van Dijken <irisvdijken@gmail.com>
2020-11-15 08:06:10 +0100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  get 'ask', to: 'questions#ask', as: :ask
-  get 'answer', to: 'questions#answer', as: :answer
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
